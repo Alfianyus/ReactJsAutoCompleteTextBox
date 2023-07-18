0c40f74ed85bdfdd76a44388c9b4c2c31cb1b27c
alfian.yuska <alfian.yuska@phiraka.com>
2023-07-18 23:01:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [allcountry, setAllcountry] = useState([]);
+    const [filterresult, setFilterresult] = useState([]);
+    const [searchcontry, setSearchcountry] = useState("");
+
+    const handlesearch = (event) => {
+        const search = event.target.value;
+        console.log(search);
+        setSearchcountry(search);
+
+        if (search !== "") {
+            const filterdata = allcountry.filter((item) => {
+                return Object.values(item)
+                    .join("")
+                    .toLowerCase()
+                    .includes(search.toLowerCase());
+            });
+            setFilterresult(filterdata);
+        } else {
+            setFilterresult(allcountry);
+        }
+
+
+
+    };
+
+    useEffect(() => {
+        const getcountry = async () => {
+            const getres = await fetch("http://127.0.0.1:8000/api/autocomplete");
+            const setcountry = await getres.json();
+            console.log(setcountry.results);
+            setAllcountry(await setcountry.results);
+        };
+        getcountry();
+    }, []);
+
+
+
+    return (
+        <div className="container">
+            <div className='row'>
+                <h3>React Js Auto complete Text Box| Filter search</h3>
+                <div className='col-md-6 mb-3 mt-3'>
+                    <input
+                        type='text'
+                        className='form-control'
+                        placeholder='Enter Keyboard'
+                        onChange={(e) => {
+                            handlesearch(e);
+                        }}
+                    />
+                    <table className='table table-bordered table-striped'>
+                        <thead>
+                            <tr>
+                                <th>County ID</th>
+                                <th>Country Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {searchcontry.length > 1
+                                ? filterresult.map((filtercountry, index) => (
+                                    <tr key={index}>
+                                        <td>{filtercountry.id}</td>
+                                        <td>{filtercountry.countryName}</td>
+                                    </tr>
+                                ))
+
+
+
+                                : allcountry.map((getcon, index) => (
+                                    <tr key={index}>
+                                        <td>{getcon.id}</td>
+                                        <td>{getcon.countryName}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
